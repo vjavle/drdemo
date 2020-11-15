a2831eb8d69c32e3baac8fd0484d1f8a3077467e
Vasant Javle <vjavle@hotmail.com>
2020-11-15 13:20:00 -0500

    Correct for each dependencies

--- infra/tf/sqlfirewall.tf
+++ infra/tf/sqlfirewall.tf
@@ -1,8 +1,8 @@
 resource "azurerm_sql_firewall_rule" "primary-sql_firewall_rule" {
     for_each = var.dr_regions
     name = format("sqlfw-%s-%s-%s", each.key, var.environment,var.application_name)
-    resource_group_name = format("rg-%s-%s-%s", each.key, var.environment,var.application_name)
-    server_name = format("sql-%s-%s-%s", each.key, var.environment,var.application_name)
+    resource_group_name = azurerm_resource_group.rg_subscription_bzunit_env_region[each.key].name 
+    server_name = azurerm_sql_server.sql_subscription_bzunit_env_region[each.key].name
     start_ip_address = var.start_ip_address
     end_ip_address = var.end_ip_address
 }
--- infra/tf/sqlserver.tf
+++ infra/tf/sqlserver.tf
@@ -1,7 +1,7 @@
-resource "azurerm_sql_server" "sql_subscription_bzunit_env_region_primary" {
+resource "azurerm_sql_server" "sql_subscription_bzunit_env_region" {
   for_each = var.dr_regions
   name                         = format("sql-%s-%s-%s", each.key, var.environment,var.application_name)
-  resource_group_name          = format("rg-%s-%s-%s", each.key, var.environment,var.application_name)
+  resource_group_name          = azurerm_resource_group.rg_subscription_bzunit_env_region[each.key].name
   location                     = each.value
   version                      = "12.0"
   administrator_login          = var.administrator_login_uid
